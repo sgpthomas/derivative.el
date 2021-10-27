bb33efc2bfc450522952fec2fd89ccacfc7a7529
Sammy Thomas <sgpthomas@gmail.com>
2021-10-26 19:55:36 -0500

    bunch of work making things work

--- derivative.el
+++ derivative.el
@@ -1,3 +1,5 @@
+;;; -*- lexical-binding: t; -*-
+
 (require 'dash)
 
 (cl-defstruct d/empty)
@@ -6,6 +8,26 @@
 (cl-defstruct d/rep lang)
 (cl-defstruct d/cat left right)
 (cl-defstruct d/alt this that)
+
+(defun d/langp (obj)
+  (or (d/empty-p obj)
+      (d/eps-p obj)
+      (d/char-p obj)
+      (d/rep-p obj)
+      (d/cat-p obj)
+      (d/alt-p obj)))
+
+(defun make-word-L (word)
+  (if (not (equal word ""))
+      (make-d/cat :left (make-d/char :value (substring word 0 1))
+		  :right (make-word-L (substring word 1 (string-width word))))
+    (make-d/eps)))
+
+(defun d/infer (obj)
+  (cl-typecase obj
+    (d/langp obj)
+    (characterp (make-d/char :value (string obj)))
+    (stringp (make-word-L obj))))
 
 (defun nullable (L)
   (cl-typecase L
@@ -42,11 +64,7 @@
 				       :right l2)
 		     :that (derivative c l2)))))))
 
-(defun make-word-L (word)
-  (if (not (equal word ""))
-      (make-d/cat :left (make-d/char :value (substring word 0 1))
-		  :right (make-word-L (substring word 1 (string-width word))))
-    (make-d/eps)))
+
 
 (defun check-word (word L)
   (if (equal word "")
@@ -57,11 +75,85 @@
 
 (check-word "hello" (make-word-L "hello"))
 
+(setq L (make-d/cat :left L
+		    :right (make-d/alt :this (make-d/char :value "a")
+				       :that (make-d/char :value "b"))))
+
+(define-lang L
+  ;; (abs (cat (word "lambda")
+  ;; 	    binder
+  ;; 	    (word ".")
+  ;; 	    expr))
+  (id (alt (word "a")
+	   (word "b")
+	   (word "c")
+	   (word "x")
+	   (word "y")
+	   (word "z")))
+  (ws (word " "))
+  (binder (cat id (rep (cat ws id))))
+  )
+
+(defun c-alt (&rest args)
+  (if (consp args)
+      (make-d/alt :this (d/infer (car args))
+		  :that (apply 'c-alt (cdr args)))
+    (make-d/eps)))
+
+(defun c-cat (&rest args)
+  (if (consp args)
+      (make-d/cat :left (d/infer (car args))
+		  :right (apply 'c-cat (cdr args)))
+    (make-d/eps)))
+
+(defun c-wscat (&rest args)
+  (apply 'c-cat (-interpose " " args)))
+
+(defun c-* (L)
+  (make-d/rep :lang L))
+
+(check-word
+ "lambda x . x"
+ (c-wscat "lambda" ?x  "." ?x))
+
+(c-alt ?a ?b ?c)
+
+(c-alt (make-d/char :value "a"))
+
+(apply 'c-alt (list (make-d/char :value "a")
+		    (make-d/char :value "b")
+		    (make-d/char :value "c")))
+
+
+(setq L
+      (let* ((id (c-alt ?a ?b ?c ?d ?e ?f ?g ?x ?y ?z))
+	     (ws " ")
+	     (binder (c-cat id (c-* (c-cat ws id))))
+	     (plus (c-wscat id "+" id))
+	     (minus (c-wscat id "-" id))
+	     (expr (c-alt plus minus))
+	     )
+	;; (c-cat id (c-* (c-cat ws id)))
+	(c-wscat "lambda" binder "." expr)
+	))
+
+(letrec ((id (c-alt ?a ?b ?c ?d ?e ?f ?g ?x ?y ?z))
+	 (plus (c-wscat expr "+" expr))
+	 (minus (c-wscat expr "-" expr))
+	 (expr (c-alt plus minus)))
+  plus
+  )
+
+
+(check-word "lambda f x y . x + y" L)
+
+lambda x y . x + x
+
 (--> (make-word-L "hell")
      (derivative "h" it)
      (derivative "e" it)
-     ;; (derivative "l" it)
-     ;; (derivative "l" it)
+     (derivative "l" it)
+     (derivative "l" it)
      ;; (derivative "o" it)
      )
 
